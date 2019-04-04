d603f7ac5168836a2ab921f390e4152fe2129c27
cjangelinatorres <cjangelinatorres@10.117.222.41>
2019-04-03 20:07:03 -0700

    Add a line to this file. -CJ Angelina

---
+++ CSC131Lab02/src/org/csus/edu/Hello.java
@@ -0,0 +1,9 @@
+package org.csus.edu;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("This line was added by CJ Angelina.");
+		
+	}
+}
