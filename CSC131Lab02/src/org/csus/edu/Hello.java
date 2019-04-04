662a003fc6ad850efd4f9070b032c8c780662fe1
Jorge Cedeno <israelcdga@gmail.com>
2019-04-03 20:11:44 -0700

    Update Hello.java

--- CSC131Lab02/src/org/csus/edu/Hello.java
+++ CSC131Lab02/src/org/csus/edu/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("This line was added by CJ Angelina.");
+		System.out.println("This line was added by Jorge Cedeno.");
+
 		
 	}
 }
