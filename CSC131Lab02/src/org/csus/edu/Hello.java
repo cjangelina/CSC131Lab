f3fa3dbb33a9a739559af4b6e528e2d86a148457
cjangelinatorres <cjangelinatorres@10.117.222.41>
2019-04-03 20:22:54 -0700

    Line added.

--- CSC131Lab02/src/org/csus/edu/Hello.java
+++ CSC131Lab02/src/org/csus/edu/Hello.java
@@ -6,7 +6,10 @@ public class Hello {
 		System.out.println("This line was added by CJ Angelina.");
 		System.out.println("This line was added by Jorge Cedeno.");
 		System.out.println("This line was added by Mary Akshara.");
-		System.out.println("This line was added by kanwaldeep kaur.");
+		System.out.println("This line was added by Kanwaldeep Kaur.");
+		System.out.println("This line was added by Austin Wilson.");
+		System.out.println("This line was added by Javier Romero.");
+		System.out.println("This line was added by Steve Nguyen.");
 		
 
 		
